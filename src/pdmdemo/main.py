a84716c3c0caa185634bb669a1bcf70c8220b392
Lewis Cowper <lewcowper@gmail.com>
2024-06-18 22:16:52 +1000

    first commit

---
+++ src/pdmdemo/main.py
@@ -0,0 +1,7 @@
+from typing import List
+
+import numpy as np
+
+
+def simplesum(x: List[int]) -> int:
+    return np.sum(x)
---
+++ tests/test_simplesum.py
@@ -0,0 +1,6 @@
+from src.pdmdemo.main import simplesum
+
+
+def test_simplesum():
+    assert simplesum([1, 2, 3]) == 6
+    assert simplesum([]) == 0
